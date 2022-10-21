ed709d90c66cb6563bbe806a49b872dcedd4f257
Deepak M S <deepakcoder80@gmail.com>
2022-10-21 20:01:19 +0530

    bresenham => output obtained => convert code to slope grater than 1

--- lines/Bresenham/main.py
+++ lines/Bresenham/main.py
@@ -2,51 +2,62 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
-title="Bresenham Line Drawing"
+title="Breseham algorithm"
 window =0
-width,height = 500,500
+width , height = 500,400
 
-def refresh2D(width,height):
+def init(width,height):
     glViewport(0,0,width,height)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     glOrtho(0,width,0,height,0,1)
+    glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
-def draw():
+def draw(x1,y1,x2,y2):
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
-    refresh2D(width,height)
+    init(width,height)
     glColor3f(1,1,0)
     glPointSize(12)
-    bresenHam(10,10,80,50)
+    ploat_line(x1,y1,x2,y2)
     glutSwapBuffers()
 
-def bresenHam(x1,y1,x2,y2):
-    x=x1
-    y=y1
-    dx=x2-x1
-    dy=y2-y1
-    p = 2*dy-dx
-    glBegin(GL_POINTS)
+
+
+
+def ploat_line(x1,y1,x2,y2):
+    x = x1;
+    y=y1;
+    dx = x2-x1
+    dy = y2-y1
+    p = (2*dy)-dx
+    glBegin(GL_POINTS)   
     while x<=x2:
         glVertex2f(x,y)
-        x=x+1
+        x = x+1
         if p<0:
-            p = p+2*dy 
+            p = p+(2*dy)
         else:
-            p=p+2*dy-2*dx
+            p= p+((2*dy)-2*dx)
             y=y+1
     glEnd()
 
 def main():
+    print("Enter the first coordinate")
+    x1 = int(input("Enter x1 here : "))
+    y1 = int(input("Enter y1 here : "))
+    print("Enter the last point coordinate")
+    x2 = int(input("Enter x2 here : "))    
+    y2 = int(input("Enter y2 here : "))
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_RGBA)
     glutInitWindowSize(width,height)
     glutInitWindowPosition(0,0)
-    window=glutCreateWindow(title)
-    glutDisplayFunc(draw)
-    glutIdleFunc(draw)
+    window = glutCreateWindow(title)
+    glutDisplayFunc(lambda:draw(x1,y1,x2,y2))
+    glutIdleFunc(lambda:draw(x1,y1,x2,y2))
     glutMainLoop()
 
 main()
+
