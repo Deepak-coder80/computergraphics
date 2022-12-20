8e1fbb739b7157405916a00225b0826d9aa222bd
Deepak M S <deepakcoder80@gmail.com>
2022-12-20 22:12:59 +0530

    boundary fill

--- polygonfilling/boudaryfill/main.py
+++ polygonfilling/boudaryfill/main.py
@@ -2,6 +2,7 @@ from OpenGL.GL import *
 from OpenGL.GLU import * 
 from OpenGL.GLUT import * 
 import sys
+import numpy as np
 
 sys.setrecursionlimit(10**6)
 
@@ -9,41 +10,49 @@ def refresh2D(width,height):
     glViewport(0,0,width,height)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    glOrtho(-100,width,-100,height,0,-1)
+    glOrtho(0,width,0,height,0,-1)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
-def bound_it( x, y, fill, bg):
-    color=[0,0,0]
-    glReadPixels(x,450-y,1.0,1.0,GL_RGB,GL_FLOAT,color)
-
-    if(
-        (color[0]!=bg[0] or color[1]!=bg[1] or color[2]!=bg[2])
-        and (color[0]!=fill[0] or color[1]!=fill[1] or color[2]!=fill[2])
-    ):
-
-        glColor3f(fill[0],fill[1],fill[2])
-        glPointSize(10)
-       
-        glBegin(GL_POINTS)
-        glVertex2f(x,450-y)
-        glEnd()
-        glFlush()
-        bound_it(x+1,y,fill,bg)
-        bound_it(x-2,y,fill,bg)
-        bound_it(x,y+2,fill,bg)
-        bound_it(x,y-2,fill,bg)
-
-
-
-def mouse(btn ,state,x,y):
+def read_pixel(x,y):
+    color = glReadPixels(x,y,1,1,GL_RGB,GL_FLOAT)
     
+    return np.array([round(x) for x in color[0][0]])
+
+def ploat_pixel(x,y):
+   
+    glBegin(GL_POINTS)
+    glVertex2f(x,y)
+    glEnd()
+    glFlush()
+
+def bound_it( x, y, fill, bg):
+    color = read_pixel(x,y)
+    print(color)
+    
+    fill = np.array(fill)
+    bg = np.array(bg)
+    if(not all(color==fill)and not all(color==bg)):
+            glColor3f(fill[0],fill[1],fill[2])
+            ploat_pixel(x,y)
+            bound_it(x+1,y,fill,bg)
+            bound_it(x-1,y,fill,bg)
+            bound_it(x,y+1,fill,bg)
+            bound_it(x,y-1,fill,bg)
+            bound_it(x+1,y+1,fill,bg)
+            bound_it(x-1,y+1,fill,bg)
+            bound_it(x+1,y-1,fill,bg)
+            bound_it(x-1,y-1,fill,bg)
+            
+            
+   
+
+def mouse(btn ,state,x,y):    
     if(btn==GLUT_LEFT_BUTTON):
-        if(state==GLUT_UP):
-            print(x,y)
+        if(state==GLUT_DOWN):
             bCol=[1,0,0]
-            color=[0,0,1]
-            bound_it(x,450-y,bCol,color)
+            color=[0,1,0]
+            bound_it(x,500-y,color,bCol)
     
   
 
@@ -55,10 +64,17 @@ def draw():
     glLineWidth(3)
     glPointSize(2)
     glColor3f(1,0,0)
-    glBegin(GL_TRIANGLES)
+    glBegin(GL_LINES)
     glVertex2f(150,100)
     glVertex2f(300,300)
+    glEnd()
+    glBegin(GL_LINES)
+    glVertex2f(300,300)
     glVertex2f(450,100)
+    glEnd()
+    glBegin(GL_LINES)
+    glVertex2f(450,100)
+    glVertex2f(150,100)
     glEnd()
     glutSwapBuffers()
 
